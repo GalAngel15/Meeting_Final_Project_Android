37a72a1905fd477c36c85e430d739b0ce31ab257
YardenCherry <yardenchery@gmail.com>
2025-03-24 18:24:28 +0200

    start Tutorial for questions activity and change the languages to english On some screens

---
+++ app/src/main/java/com/example/meeting_project/activities/TutorialForQuestions.java
@@ -0,0 +1,21 @@
+package com.example.meeting_project.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.meeting_project.R;
+
+public class TutorialForQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_for_questions);
+
+    }
+}
