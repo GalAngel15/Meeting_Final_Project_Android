8cb8cd4191f5218ec7ae897842441a820f31e07c
GalAngel15 <galangel151@gmail.com>
2025-04-03 00:40:46 +0300

    03.04.2025

--- app/src/main/java/com/example/meeting_project/ApiClient.java
+++ app/src/main/java/com/example/meeting_project/ApiClient.java
@@ -7,9 +7,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    //private static final String BASE_URL = "http://192.168.1.166:3000/";
-    //private static final String BASE_URL = "http://10.0.2.2:3000/";
-    private static final String BASE_URL = "http://192.168.56.1:3000/";
+    private static final String BASE_URL = "https://16personalities-api-production.up.railway.app/";
+
     private static Retrofit retrofit= null;
 
     public static synchronized Retrofit getRetrofitInstance() {
