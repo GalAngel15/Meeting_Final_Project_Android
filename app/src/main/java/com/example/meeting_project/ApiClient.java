5424f21a433aaa4949d26dc8e111f0198c292478
YardenCherry <yardenchery@gmail.com>
2025-04-10 23:24:45 +0300

    fix the 16personality API

--- app/src/main/java/com/example/meeting_project/ApiClient.java
+++ app/src/main/java/com/example/meeting_project/ApiClient.java
@@ -7,8 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://16personalities-api-production.up.railway.app/";
-
+    private static final String BASE_URL = "http://192.168.56.1:3000/";
     private static Retrofit retrofit= null;
 
     public static synchronized Retrofit getRetrofitInstance() {
