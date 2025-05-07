a2ed3530ed4e21e4ca3d2d3bba8f8c7c6b8cfde1
yahavLer <118624372+yahavLer@users.noreply.github.com>
2025-05-07 08:28:09 +0300

    save answer working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,4 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
 
-    //svg
-    implementation ("com.github.corouteam:GlideToVectorYou:v2.0.0")
-    implementation ("com.github.qoqa:glide-svg:1.0.5")
-
 }
--- app/src/main/java/com/example/meeting_project/apiClients/MbtiTest_ApiClient.java
+++ app/src/main/java/com/example/meeting_project/apiClients/MbtiTest_ApiClient.java
@@ -8,8 +8,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MbtiTest_ApiClient {
 //    private static final String BASE_URL = "http://192.168.56.1:3000/";
-    private static final String BASE_URL = "http://192.168.1.166:3000/";
-//      private static final String BASE_URL = "http://192.168.154.1:3000/";
+//    private static final String BASE_URL = "http://192.168.1.166:3000/";
+//    private static final String BASE_URL = "http://192.168.154.1:3000/";
+    private static final String BASE_URL = "http://10.0.2.2:3000/";
 
     private static Retrofit retrofit= null;
 
