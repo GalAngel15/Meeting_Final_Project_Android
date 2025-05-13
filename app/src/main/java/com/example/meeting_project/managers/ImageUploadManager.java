f4cc7381fa1e67f640a96c3890bc72a0a8cf9814
GalAngel15 <galangel151@gmail.com>
2025-05-13 11:15:53 +0300

    Photos

--- app/src/main/java/com/example/meeting_project/managers/ImageUploadManager.java
+++ app/src/main/java/com/example/meeting_project/managers/ImageUploadManager.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+
 public class ImageUploadManager {
     public interface UploadCallback {
         void onSuccess(String imageUrl);
@@ -25,5 +28,37 @@ public class ImageUploadManager {
                     callback.onSuccess(downloadUrl);
                 }))
                 .addOnFailureListener(callback::onFailure);
+    }
+
+    public static void uploadMultipleImages(Context context, ArrayList<Uri> imageUris, String userId, MultipleUploadCallback callback) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        ArrayList<String> imageUrls = new ArrayList<>();
+        StorageReference userRef = storage.getReference()
+                .child("Users")
+                .child(userId)
+                .child("gallery");
+
+        int[] uploadedCount = {0};
+        for (int i = 0; i < imageUris.size(); i++) {
+            Uri uri = imageUris.get(i);
+            String imageName = "img" + i + ".jpg";
+            StorageReference imageRef = userRef.child(imageName);
+
+            imageRef.putFile(uri)
+                    .addOnSuccessListener(taskSnapshot ->
+                            imageRef.getDownloadUrl().addOnSuccessListener(uriResult -> {
+                                imageUrls.add(uriResult.toString());
+                                uploadedCount[0]++;
+                                if (uploadedCount[0] == imageUris.size()) {
+                                    callback.onSuccess(imageUrls);
+                                }
+                            }))
+                    .addOnFailureListener(callback::onFailure);
+        }
+    }
+
+    public interface MultipleUploadCallback {
+        void onSuccess(ArrayList<String> imageUrls);
+        void onFailure(Exception e);
     }
 }
