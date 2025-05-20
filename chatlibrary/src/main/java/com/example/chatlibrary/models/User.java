b6119150ce6a691524efad9448635feac07dde45
YardenCherry <yardenchery@gmail.com>
2025-05-20 17:16:34 +0300

    Chat library

---
+++ chatlibrary/src/main/java/com/example/chatlibrary/models/User.java
@@ -0,0 +1,40 @@
+package com.example.chatlibrary.models;
+
+public class User {
+    private String id;
+    private String username;
+    private String passwordHash;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
