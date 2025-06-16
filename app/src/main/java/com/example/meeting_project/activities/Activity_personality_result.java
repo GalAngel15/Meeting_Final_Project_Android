1e86b5fea4d048542109bf7fb1be815f8070a465
yahavLer <118624372+yahavLer@users.noreply.github.com>
2025-06-16 17:32:58 +0300

    Activity_personality_result add menu

--- app/src/main/java/com/example/meeting_project/activities/Activity_personality_result.java
+++ app/src/main/java/com/example/meeting_project/activities/Activity_personality_result.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
@@ -18,23 +24,77 @@ import com.example.meeting_project.GlideAdapter.GlideApp;
 import com.example.meeting_project.GlideAdapter.SvgSoftwareLayerSetter;
 import com.example.meeting_project.R;
 import com.example.meeting_project.managers.AppManager;
+import com.example.meeting_project.managers.NevigationActivity;
 import com.example.meeting_project.objectOfMbtiTest.SubmitResponse;
 import com.example.meeting_project.objectOfMbtiTest.Trait;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Activity_personality_result extends AppCompatActivity {
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    private ImageButton menuButton;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
 
     private TextView tvNiceName, tvSnippet;
     private ImageView ivAvatar;
     private LinearLayout scalesContainer, traitsContainer;
     private Button btnNextTest;
+
+    private static final Map<Integer, Class<?>> NAV_MAP = new HashMap<>();
+    static {
+        NAV_MAP.put(R.id.nav_edit_preferences, activity_preferences.class);
+        NAV_MAP.put(R.id.nav_edit_intro, Activity_questionnaire.class);
+        NAV_MAP.put(R.id.nav_my_personality, PersonalitiesActivity.class);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personality_result);
         initViews();
+        // הגדר את ה־NavigationView
+        menuButton.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+        NevigationActivity.findNevigationButtens(this);
+
+        // 2. הגדרת ה-Toolbar כ-ActionBar
+        setSupportActionBar(toolbar);
+
+        // 3. יצירת ה-Toggle והוספתו ל-DrawerLayout
+        toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,   // ודא שיש לך מחרוזות אלו ב־strings.xml
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // טיפול בלחיצות תפריט צד
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            Class<?> targetActivity = NAV_MAP.get(itemId);
+
+            if (targetActivity != null) {
+                Intent intent = new Intent(Activity_personality_result.this, targetActivity);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "אין פעולה מתאימה", Toast.LENGTH_SHORT).show();
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
         setBtnClick();
         // קבל את ה־JSON
         String json = getIntent().getStringExtra("submitResponseJson");
@@ -134,6 +194,11 @@ public class Activity_personality_result extends AppCompatActivity {
         scalesContainer = findViewById(R.id.scalesContainer);
         traitsContainer = findViewById(R.id.traitsContainer);
         btnNextTest = findViewById(R.id.btnNextTest);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        menuButton = findViewById(R.id.btn_menu);
+        navigationView = findViewById(R.id.navigation_view);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        toolbar = findViewById(R.id.toolbar);
     }
 
 }
