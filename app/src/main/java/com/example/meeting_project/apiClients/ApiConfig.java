9cd369027c259163d3e2768e5bf6ec6c634002b0
yahavLer <118624372+yahavLer@users.noreply.github.com>
2025-08-03 17:38:32 +0300

    update api client

--- app/src/main/java/com/example/meeting_project/apiClients/ApiConfig.java
+++ app/src/main/java/com/example/meeting_project/apiClients/ApiConfig.java
@@ -2,5 +2,9 @@ package com.example.meeting_project.apiClients;
 
 public class ApiConfig {
     public static final String BASE_IP = "http://192.168.1.164";
+    //yahav home wifi
+    //public static final String BASE_IP = "http://192.168.68.100/";
+    //emulator address
+    //public static final String BASE_URL = "http://10.0.0.2/";
 
 }
