1292a6ad810e3f13f56c4439bde3296e174f278d
GalAngel15 <galangel151@gmail.com>
2025-08-09 22:11:32 +0300

    temp

--- app/src/main/java/com/example/meeting_project/apiClients/ApiConfig.java
+++ app/src/main/java/com/example/meeting_project/apiClients/ApiConfig.java
@@ -1,9 +1,9 @@
 package com.example.meeting_project.apiClients;
 
 public class ApiConfig {
-    //public static final String BASE_IP = "http://192.168.1.164";
+    public static final String BASE_IP = "http://192.168.1.89";
     //yahav home wifi
-    public static final String BASE_IP = "http://192.168.68.100";
+    //public static final String BASE_IP = "http://192.168.68.100";
     //emulator address
     //public static final String BASE_IP = "http://10.0.0.2";
     //diana home wifi
